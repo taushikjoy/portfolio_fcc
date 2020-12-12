4a730b298aacf4e7d1e41bc5f3f593307395a018
Taushik Ahmed Joy <taushikjoy@gmail.com>
2020-12-12 23:07:00 +0600

    commiting protfolio for fcc

---
+++ joy.js
@@ -0,0 +1,18 @@
+
+let y=x-1 ;
+
+var n = prompt("Enter input: ");
+
+for (let x =2 ; x<100 ; x++) {
+     
+
+        if (((x*x*x) - (y*y*y)) === n ) {
+             let u = x ;
+             let v = y ;
+        }  
+}
+console.log("x is : "+ u + " y is : " + v);
+
+
+
+
